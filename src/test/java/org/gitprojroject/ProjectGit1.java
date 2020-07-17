83ad656ff68c13cf3ab7eb0aa52caffb80eb92e2
jyothi0911 <sivanagajyothi522@gmail.com>
2020-07-17 14:13:10 +0530

    task 1

---
+++ src/test/java/org/gitprojroject/ProjectGit1.java
@@ -0,0 +1,16 @@
+package org.gitprojroject;
+
+import java.util.Scanner;
+
+public class ProjectGit1 {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.println("enter the value");
+		 String s=sc.next();
+		System.out.println(s);
+		
+		
+	}
+
+}
