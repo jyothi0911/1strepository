1aea78581a6c83f1a89bed07e4e9785f2894046f
jyothi0911 <sivanagajyothi522@gmail.com>
2020-07-20 22:04:12 +0530

    task completd

--- src/test/java/org/gitprojroject/ProjectGit1.java
+++ src/test/java/org/gitprojroject/ProjectGit1.java
@@ -3,14 +3,21 @@ package org.gitprojroject;
 import java.util.Scanner;
 
 public class ProjectGit1 {
+	
+	
+	
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
 		
 		System.out.println("enter the value");
 		 String s=sc.next();
 		System.out.println(s);
+		int a=200;
+		int b=300;
+		System.out.println(a);
 		
 		
 	}
 
 }
+
